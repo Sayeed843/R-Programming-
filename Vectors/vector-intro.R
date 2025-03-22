c85dd5a739daddd66bd1836f8ed5b606474c31e4
Sayeed843 <sayeed.swe@gmail.com>
2025-03-22 11:46:11 +0100

    R programming vector

---
+++ Vectors/vector-intro.R
@@ -0,0 +1,56 @@
+# CREATE
+cities_population <- c(10000, 20000, 4000, 3000)
+
+# REMOVE  THE OBJECT
+# rm(cities_population)
+
+char_cities_population <- c(10000, 290000, 4000, '3000')
+sum(cities_population)
+
+# THROW AN ERROR FOR CHARACTERS
+#sum(char_cities_population)  
+
+
+# VECTOR INDEXING AND SLICING
+melons <- c(1.2, 1.4, 2.4, 3.4)
+
+# INDEXING BY POSITION
+melons[1]
+melons[2]
+melons[3]
+
+# OUT OF THE RANGE
+melons[5]
+
+# Slicing
+melons[2:4]
+melons[1:2]
+
+# NOW WE WILL RETRIVE THE 1st and 4th Elements ONLY
+melons[c(1,4)]
+
+
+# Vector Operations
+melons[1] + melons[2]+melons[3]+melons[4]
+
+
+sum(melons)
+
+melons*2
+
+melons/100
+
+melons+melons 
+
+melons^2
+
+sqrt(melons)
+
+exp(melons)
+
+# MULTILY TWO DIFFERENT VECTORS
+adjustment_weight = c(1.2, 1.2, 1.4, 1.4)
+
+melons * adjustment_weight
+
+melons[1]
---
+++ exercise-1.R
@@ -0,0 +1,12 @@
+# Use R to extract the square root of 85
+sqrt(85)
+
+# Compute the following equation in R
+15/(10^2)
+
+# Compute the following equation in R
+(215+log(100))/(exp(100))
+
+# Use R to compute the natural logarithm of 24
+log(24)
+?log()
